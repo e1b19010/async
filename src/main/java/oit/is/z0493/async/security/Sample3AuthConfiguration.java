47be34ad9308ac235e7f9f6f6c553243bbbecfe8
okaki713 <e1b19014@oit.ac.jp>
2021-10-26 14:31:17 +0900

    DB設定

--- src/main/java/oit/is/z0493/async/security/Sample3AuthConfiguration.java
+++ src/main/java/oit/is/z0493/async/security/Sample3AuthConfiguration.java
@@ -45,5 +45,8 @@ public class Sample3AuthConfiguration extends WebSecurityConfigurerAdapter {
 
     // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8000/ に戻る
     http.logout().logoutSuccessUrl("/");
+
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
   }
 }
